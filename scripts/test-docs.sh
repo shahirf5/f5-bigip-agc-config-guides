9ea1ebfe7790066db9a84a6b37c83c58e60cf83d
Jodie Putrino <j.putrino@f5.com>
2017-12-04 15:56:03 -0700

    Fix deployment to s3 - moved build and source directories under new /docs directory to match path expected by deployment script
    Remove redundant steps from make commands
    
    change deployment script back to correct repo and branch

--- scripts/test-docs.sh
+++ scripts/test-docs.sh
@@ -2,20 +2,21 @@
 # Test docs build, grammar, and links
 # can be run standalone or with docker-docs.sh
 
-set -x
+set -e
 
+echo -e "Building docs with Sphinx ... \n"
+make docs
+
+echo -e "Checking links\n"
+make linkcheck || true
+
+echo -e "Renaming doc files using camel-case for grammar check\n"
+set -x
 # Rename doc files using camel-case (required for write-good grammar check)
 rename -e 'y/\ /_/' source/saml-idp-connector/*.rst
 rename -e 'y/\ /_/' source/saml-saas-applications/*.rst
 
-set -e
-
-echo "Building docs with Sphinx"
-make clean
-make html
-
 echo "Checking grammar and style"
-write-good `find source -iname '*.rst'` --passive --so --no-illusion --thereIs --cliches
 
-echo "Checking links"
-make linkcheck || true
+write-good `find docs/source -iname '*.rst'` --passive --so --no-illusion --thereIs --cliches
+
